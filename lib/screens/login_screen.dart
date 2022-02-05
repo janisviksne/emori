1023192cdd3c39e7cd0225a595cdc1d7b566b936
Jānis Vīksne <viksnejanis7@gmail.com>
2022-02-05 11:54:51 +0200

    Addition of login screen w/ buttons, input fields

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,29 +1,94 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
   @override
-  State<StatefulWidget> createState() {
-    return _LoginScreenState();
-  }
+  State<StatefulWidget> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text(
-              'Welcome to Emori',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 50.0,
+      //padding: const EdgeInsets.all(10),
+      body: ListView(
+        children: <Widget>[
+          Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                'Amori',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 30),
+              )),
+          Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                'Sign in',
+                style: TextStyle(fontSize: 20),
+              )),
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: TextField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'User Name',
               ),
             ),
-          ],
-        ),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+            child: TextField(
+              obscureText: true,
+              controller: passwordController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              //forgot password screen
+            },
+            child: const Text(
+              'Forgot Password',
+            ),
+          ),
+          Container(
+              height: 50,
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: ElevatedButton(
+                child: const Text('Login'),
+                onPressed: () {
+                  print(nameController.text);
+                  print(passwordController.text);
+                },
+              )),
+          Row(
+            children: <Widget>[
+              const Text('Don\'t not have account?'),
+              TextButton(
+                child: const Text(
+                  'Sign up',
+                  style: TextStyle(fontSize: 20),
+                ),
+                onPressed: () {
+                  //signup screen
+                },
+              )
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+        ],
       ),
     );
   }
