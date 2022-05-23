25a07cfe32fd47990b5d62f08dd0d88f8b1136b1
Jānis Vīksne <viksnejanis7@gmail.com>
2022-05-23 22:58:19 +0200

    Added embedded database for Love myself answer task

---
+++ lib/floor_database/love_myself_answer_database.g.dart
@@ -0,0 +1,129 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'love_myself_answer_database.dart';
+
+// **************************************************************************
+// FloorGenerator
+// **************************************************************************
+
+// ignore: avoid_classes_with_only_static_members
+class $FloorAppDatabase {
+  /// Creates a database builder for a persistent database.
+  /// Once a database is built, you should keep a reference to it and re-use it.
+  static _$AppDatabaseBuilder databaseBuilder(String name) =>
+      _$AppDatabaseBuilder(name);
+
+  /// Creates a database builder for an in memory database.
+  /// Information stored in an in memory database disappears when the process is killed.
+  /// Once a database is built, you should keep a reference to it and re-use it.
+  static _$AppDatabaseBuilder inMemoryDatabaseBuilder() =>
+      _$AppDatabaseBuilder(null);
+}
+
+class _$AppDatabaseBuilder {
+  _$AppDatabaseBuilder(this.name);
+
+  final String? name;
+
+  final List<Migration> _migrations = [];
+
+  Callback? _callback;
+
+  /// Adds migrations to the builder.
+  _$AppDatabaseBuilder addMigrations(List<Migration> migrations) {
+    _migrations.addAll(migrations);
+    return this;
+  }
+
+  /// Adds a database [Callback] to the builder.
+  _$AppDatabaseBuilder addCallback(Callback callback) {
+    _callback = callback;
+    return this;
+  }
+
+  /// Creates the database and initializes it.
+  Future<AppDatabase> build() async {
+    final path = name != null
+        ? await sqfliteDatabaseFactory.getDatabasePath(name!)
+        : ':memory:';
+    final database = _$AppDatabase();
+    database.database = await database.open(
+      path,
+      _migrations,
+      _callback,
+    );
+    return database;
+  }
+}
+
+class _$AppDatabase extends AppDatabase {
+  _$AppDatabase([StreamController<String>? listener]) {
+    changeListener = listener ?? StreamController<String>.broadcast();
+  }
+
+  LoveMyselfAnswerDao? _loveMyselfAnswerDaoInstance;
+
+  Future<sqflite.Database> open(String path, List<Migration> migrations,
+      [Callback? callback]) async {
+    final databaseOptions = sqflite.OpenDatabaseOptions(
+      version: 1,
+      onConfigure: (database) async {
+        await database.execute('PRAGMA foreign_keys = ON');
+        await callback?.onConfigure?.call(database);
+      },
+      onOpen: (database) async {
+        await callback?.onOpen?.call(database);
+      },
+      onUpgrade: (database, startVersion, endVersion) async {
+        await MigrationAdapter.runMigrations(
+            database, startVersion, endVersion, migrations);
+
+        await callback?.onUpgrade?.call(database, startVersion, endVersion);
+      },
+      onCreate: (database, version) async {
+        await database.execute(
+            'CREATE TABLE IF NOT EXISTS `LoveMyselfAnswer` (`answerId` INTEGER NOT NULL, `answerTitle1` TEXT NOT NULL, `answerTitle2` TEXT NOT NULL, `answerTitle3` TEXT NOT NULL, `answerTitle4` TEXT NOT NULL, `answerTitle5` TEXT NOT NULL, `answerTitle6` TEXT NOT NULL, `answerTitle7` TEXT NOT NULL, `answerTitle8` TEXT NOT NULL, `answerDateTime` TEXT NOT NULL, PRIMARY KEY (`answerId`))');
+
+        await callback?.onCreate?.call(database, version);
+      },
+    );
+    return sqfliteDatabaseFactory.openDatabase(path, options: databaseOptions);
+  }
+
+  @override
+  LoveMyselfAnswerDao get loveMyselfAnswerDao {
+    return _loveMyselfAnswerDaoInstance ??=
+        _$LoveMyselfAnswerDao(database, changeListener);
+  }
+}
+
+class _$LoveMyselfAnswerDao extends LoveMyselfAnswerDao {
+  _$LoveMyselfAnswerDao(this.database, this.changeListener)
+      : _loveMyselfAnswerInsertionAdapter = InsertionAdapter(
+            database,
+            'LoveMyselfAnswer',
+            (LoveMyselfAnswer item) => <String, Object?>{
+                  'answerId': item.answerId,
+                  'answerTitle1': item.answerTitle1,
+                  'answerTitle2': item.answerTitle2,
+                  'answerTitle3': item.answerTitle3,
+                  'answerTitle4': item.answerTitle4,
+                  'answerTitle5': item.answerTitle5,
+                  'answerTitle6': item.answerTitle6,
+                  'answerTitle7': item.answerTitle7,
+                  'answerTitle8': item.answerTitle8,
+                  'answerDateTime': item.answerDateTime
+                });
+
+  final sqflite.DatabaseExecutor database;
+
+  final StreamController<String> changeListener;
+
+  final InsertionAdapter<LoveMyselfAnswer> _loveMyselfAnswerInsertionAdapter;
+
+  @override
+  Future<void> insertLoveMyselfAnswer(LoveMyselfAnswer loveMyselfAnswer) async {
+    await _loveMyselfAnswerInsertionAdapter.insert(
+        loveMyselfAnswer, OnConflictStrategy.abort);
+  }
+}
